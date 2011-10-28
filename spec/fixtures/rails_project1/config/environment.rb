6872bd6e7b83f44a0a6db6daf472d74303e5695d
Sam Woodard <sam.h.woodard@gmail.com>
2011-10-27 18:37:18 -0700

    removed commented out code, recreate rakefile

--- spec/fixtures/rails_project1/config/environment.rb
+++ spec/fixtures/rails_project1/config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-# SpritesRailsTestApp::Application.initialize!
-
 require 'rake'
 include Rake::DSL
 SpritesRailsTestApp::Application.load_tasks
