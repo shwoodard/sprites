fb52353772a14ec56eb1597bff5cb41b747dd24a
Sam Woodard <sam.h.woodard@gmail.com>
2011-10-29 11:32:14 -0700

    move rake task

--- lib/sprites/rails/railtie.rb
+++ lib/sprites/rails/railtie.rb
@@ -1,32 +1,10 @@
-require 'rake/dsl_definition'
-require 'rake'
 require 'rails/railtie'
 require 'sprites'
 
 module Sprites
   class Railtie < Rails::Railtie
-
     rake_tasks do
-      include Rake::DSL
-
-      desc "Generate sprites and stylesheets"
-      task :sprites => :environment do
-        assets_root = Rails.application.config.assets[:enabled] ? 'app/assets' : 'public'
-
-        ::Sprites.configure do
-          sprites_path Rails.root.join(assets_root, 'images/sprites')
-          sprite_stylesheets_path Rails.root.join(assets_root, 'stylesheets/sprites')
-          sprite_pieces_path Rails.root.join(assets_root, 'images/sprite_images')
-        end unless ::Sprites.configuration.configured?
-
-        unless (def_file_path = Rails.root.join('config/sprites.rb')).exist?
-          raise CliApplication::DefinitionFileNotFound
-        end
-
-        load def_file_path
-        sprite_generator = ChunkyPngGenerator.new(::Sprites.configuration)
-        sprite_generator.generate(::Sprites.application.sprites)
-      end
+      load File.expand_path('../sprites.rake', __FILE__)
     end
   end
 end
---
+++ lib/sprites/rails/sprites.rake
@@ -0,0 +1,20 @@
+include Rake::DSL if defined?(Rake::DSL)
+
+desc "Generate sprites and stylesheets"
+task :sprites => :environment do
+  assets_root = Rails.application.config.assets[:enabled] ? 'app/assets' : 'public'
+
+  ::Sprites.configure do
+    sprites_path Rails.root.join(assets_root, 'images/sprites')
+    sprite_stylesheets_path Rails.root.join(assets_root, 'stylesheets/sprites')
+    sprite_pieces_path Rails.root.join(assets_root, 'images/sprite_images')
+  end unless ::Sprites.configuration.configured?
+
+  unless (def_file_path = Rails.root.join('config/sprites.rb')).exist?
+    raise CliApplication::DefinitionFileNotFound
+  end
+
+  load def_file_path
+  sprite_generator = ChunkyPngGenerator.new(::Sprites.configuration)
+  sprite_generator.generate(::Sprites.application.sprites)
+end
