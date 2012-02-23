81281e962994f3b908d2b934407b1c33e1bdfa98
Sam Woodard <sam.h.woodard@gmail.com>
2012-02-23 13:26:46 -0800

    forgot the spec

---
+++ spec/lib/sprites/railtie_spec.rb
@@ -0,0 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+require 'spec_helper'
+require 'rake'
+require 'sprites/test/sprite_generator_tester'
+require 'fixtures/rails_project1/config/application'
+
+describe Sprites::Railtie do
+  before do
+    SpritesRailsTestApp::Application.initialize!
+  end
+  
+
+  it 'should raise an Sprites::CliApplication::DefinitionFileNotFound if no sprite def file is found' do
+    Pathname.any_instance.expects(:exist?).returns(false)
+    expect { Rake::Task['sprites'].invoke }.to raise_error(Sprites::CliApplication::DefinitionFileNotFound)
+  end
+end
