3e97d7b43947b0084c6d9a71a0d3e30d25a5d74f
Sam Woodard <sam.h.woodard@gmail.com>
2012-02-25 10:05:29 -0800

    all specs pass

--- spec/lib/sprites/test/sprite_generator_tester_spec.rb
+++ spec/lib/sprites/test/sprite_generator_tester_spec.rb
@@ -3,14 +3,16 @@ require 'sprites/test/sprite_generator_tester'
 
 describe Sprites::SpriteGeneratorTester do
   describe "#test_generator" do
+    let(:sprites) { Sprites.new }
+
     it 'should return 0 for known good sprite' do
-      Sprites.configure do
-        config.sprites_path = 'spec/fixtures/known_good/sprites'
-        config.sprite_stylesheets_path = 'spec/fixtures/known_good/stylesheets'
-        config.sprite_pieces_path = 'spec/fixtures/known_good/sprite_images'
-      end
+      sprites.configuration.configure(
+        :sprites_path => 'spec/fixtures/known_good/sprites',
+        :sprite_stylesheets_path => 'spec/fixtures/known_good/stylesheets',
+        :sprite_pieces_path => 'spec/fixtures/known_good/sprite_images'
+      )
       define_buttons_sprite
-      tester = Sprites::SpriteGeneratorTester.new(Sprites.application.sprites[:buttons], Sprites.configuration)
+      tester = Sprites::SpriteGeneratorTester.new(sprites[:buttons], sprites.configuration)
       tester.should be_accurate
     end
   end
